4ffcb9bd26f80c0a093cc23431768f16583a6828
Ryota Itagaki <ribitamana@gmail.com>
2022-12-04 22:01:22 +0900

    husky導入

--- components/header.tsx
+++ components/header.tsx
@@ -1,11 +1,16 @@
-import Link from "next/link";
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
 
 const Header = () => (
   <header className="shadow-lg">
     <Link href="/">
       <h1 className="text-3xl font-bold underline">
-        <Image src="/images/mithra_logo_white.png" alt="" width={100} height={100} />
+        <Image
+          src="/images/mithra_logo_white.png"
+          alt=""
+          width={100}
+          height={100}
+        />
       </h1>
     </Link>
   </header>
