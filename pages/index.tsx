2a2598dc91e7dc82020a9fa240b2b58abdbf6e6d
Ryota Itagaki <ribitamana@gmail.com>
2022-12-13 11:21:25 +0900

    トップページ　カード並べる

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
+import PostsList from '../components/postsList';
 import Template from '../components/template';
 
 const Home = () => (
   <Template>
-    <p>ここにコンテンツを入れる</p>
-    <div>
-      <p>こんな感じ</p>
-    </div>
+    <PostsList />
   </Template>
 );
 
